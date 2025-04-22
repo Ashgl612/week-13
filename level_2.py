f0bc0f969acee2ef3983880609d083a6138a7aa2
mahshad golzarian <mahshad@mahshads-MacBook-Pro.local>
2025-04-22 18:35:39 -0400

    level 2 compelte

---
+++ level_2.py
@@ -0,0 +1,4 @@
+mix_of_stuff = [ 2, 2.3, True, "not true"]
+playlist_for_coding= [ "I trawl the megahertz by prefab sprout", "Boomerang by Elliot Moss", "I'm not okay by My Chemical Romance", "falaise by floating points", "A map,a string, a light by Yvette Young"]
+print(playlist_for_coding[0])
+playlist_for_coding.append("over-the-ocean call by Lizzy McAlpine")
